7d1d233e0a608e61def3c255d3ddc9cebaa7eced
Omolade Ajala <omoladeajala2021@gmail.com>
2025-08-27 07:26:34 +0100

    Refactor code structure for improved readability and maintainability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ function App() {
+return (
+  <div className="card">
+    <Avater />
+    <div className="data">
+      <Intro/>
+      
+      <SkillList />
+    </div>
+  </div>
+);
+}
+
+function Avater() {
+   return <img className="avater" src="omolade.jpg"  alt="Ajala Omolade" />;
+}
+
+function Intro() {
+  return   (
+ <div>
+<h1 className="header">Ajala Omolade</h1>
+<p>
+ Aspiring Software Developer and also a student at Udemy.
+  When I'm not learning a course, I do other things like watching
+  movies, making hair for people and also making Graphic Design.
+</p>
+</div>
+);    
+}
+
+function SkillList() {
+return (
+  <div>
+    <Skill skill="React" emoji="💪"/>
+    <Skill skill="HTML" emoji="💪"/>
+    <Skill skill="CSS" emoji="💪"/>
+    <Skill skill="JavaScript" emoji="💪"/>
+  </div>
+);
+}
+
+function Skill(props) {
+return (
+  <div className="skill"> 
+    <span>{props.skill}</span>
+    <span>{props.emoji}</span>
+    </div>
+);
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+ 
 root.render(
-  <React.StrictMode>
+   <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
